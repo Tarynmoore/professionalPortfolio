e810ef7bbee88764e71332064a2eefdfb8762ad4
Taryn Moore <tarynmoore628@gmail.com>
2022-11-15 19:56:12 -0700

    put some of my personal stuff in. Also worked more on the layout and need to create separate pages for my work and resume.

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,16 +13,23 @@ export default class Header extends Component {
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
              <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+               <li><a className="smoothscroll" href="#portfolio">Work</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
          </nav>
 
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}.</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
+               <h1 className="responsive-headline">
+                I'm Taryn Moore.
+                </h1>
+               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>
+                Welcome! I am a frontend-backend web developer. 
+                A couple of years ago I was told by a palm reader that I needed to do 
+                a career that constantly involved me learning, so I chose coding! 
+                Here you will find showcases of my work, and what languages I am best at. 
+                Please contact with any questions.
+
                </h3>
                <hr/>
                <ul className="social">
